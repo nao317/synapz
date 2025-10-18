0b5196fc09ceb007fb6c94ada4b245d3f815780f
morishun045 <morito.shun8051@mail.kyutech.jp>
2025-10-18 13:54:09 +0900

    ログインページのonSubmitの機能を追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,17 +3,31 @@
 // login.tsx
 import React, { useState } from 'react';
 import styles from './login.module.css';
+import { post } from '../../lib/api';
 import TypewriterText from '../../lib/components/TypewriterText';
+
 export default function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleSubmit =async (e: React.FormEvent) => {
+        e.preventDefault();
+        const data = await post("/login", { email, password });
+        if (data.token) {
+            localStorage.setItem("token", data.token);
+            alert("ログイン成功");
+            window.location.href = "/dashboard";
+        } else {
+            alert("ログイン失敗");
+        }
+    };
 
     return (
         <div className={styles.container}>
             <TypewriterText>
                 <h1 className={styles.title}>Log In</h1>
             </TypewriterText>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <input
                     type="email"
                     placeholder="Email"
