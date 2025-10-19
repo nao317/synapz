563475e54218e6c1cf5e25124438d673feb1aa0e
morishun045 <morito.shun8051@mail.kyutech.jp>
2025-10-19 11:32:16 +0900

    アイコン画像が出るところをつくりました

--- src/lib/components/PostCard.tsx
+++ src/lib/components/PostCard.tsx
@@ -2,22 +2,36 @@
 
 import React, { useState } from "react";
 import styles from "./PostCard.module.css";
+import Image from "next/image";
 
 type PostCardProps = {
-    userIconUrl: string;
+    userIconUrl: string | null | undefined;
     username: string;
     content: string;
 };
 
+const Default_user_icon = "/defaulticon.png";
+
 export default function PostCard({ userIconUrl, username, content }: PostCardProps) {
+
+    const initialSrc = userIconUrl || Default_user_icon;
+    const [iconSrc, setIconSrc] = useState(initialSrc);
+
     return (
         <div className = {styles.postCard}>
             <div className = {styles.header}>
                 <div>
-                    <img 
-                        src="{userIconUrl}" 
-                        alt='${username} Icon' 
-                        className={styles.userIcon} 
+                    <Image 
+                        src={iconSrc} 
+                        alt={`${username} Icon`} 
+                        className={styles.userIcon}
+                        width={48}
+                        height={48}
+                        onError={()=>{
+                            if (iconSrc !== Default_user_icon){
+                                setIconSrc(Default_user_icon)
+                            }
+                        }} 
                     />
                 </div>
                 <span className = {styles.username}>
