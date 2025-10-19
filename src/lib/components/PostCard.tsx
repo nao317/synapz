f7ececceaddfc78ce3b48beacdd08155d1116c27
morishun045 <morito.shun8051@mail.kyutech.jp>
2025-10-19 16:35:11 +0900

    いいねボタンを押すと色がつきカウントが増えるようにした

--- src/lib/components/PostCard.tsx
+++ src/lib/components/PostCard.tsx
@@ -2,21 +2,39 @@
 
 import React, { useState } from "react";
 import styles from "./PostCard.module.css";
+import { Button } from "./Button";
 import Image from "next/image";
+import { ThumbsUp } from "lucide-react"; 
 
 type PostCardProps = {
+    postId: string;
     userIconUrl: string | null | undefined;
     username: string;
     content: string;
+    initialIsLiked: boolean;
+    initialLikeCount: number;
 };
 
 const Default_user_icon = "/defaultIcon.png";
 
-export default function PostCard({ userIconUrl, username, content }: PostCardProps) {
+export default function PostCard({
+    postId,
+    userIconUrl,
+    username, 
+    content, 
+    initialIsLiked,
+    initialLikeCount
+  }: PostCardProps) {
 
     const initialSrc = userIconUrl || Default_user_icon;
     const [iconSrc, setIconSrc] = useState(initialSrc);
+    const [isLiked, setIsLiked] = useState(initialIsLiked);
+    const [likeCount, setLikeCount] = useState(initialLikeCount || 0);
 
+    const handleLikeClick = () => {
+        setIsLiked(!isLiked);
+        setLikeCount(isLiked ? likeCount - 1 : likeCount + 1);
+    }
     return (
         <div className = {styles.postCard}>
             <div className = {styles.header}>
@@ -40,6 +58,17 @@ export default function PostCard({ userIconUrl, username, content }: PostCardPro
             </div>
             <div className = {styles.content}>
                 {content}
+            </div>
+            <div className={styles.footer}>
+                <button onClick={handleLikeClick} className={styles.likebutton}>
+                    <ThumbsUp 
+                        size={25}
+                        stroke="white"
+                        fill={isLiked ? "#3b82f6": "none"}
+                        
+                    />
+                    <span className={styles.likecount}>{likeCount}</span>
+                </button>
             </div>
         </div>
     )
