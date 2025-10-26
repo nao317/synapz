4b6707809ca97b8840c401c4fa778da7e0ee8600
nao317 <country.gentleman.0317@gmail.com>
2025-10-26 09:35:28 +0900

    SNS部分の完成

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,11 +3,12 @@
 import React from 'react';
 import Dashboard from './dashboard';
 import TimeLine from '@/lib/components/TimeLine';
+import styles from './dashboard.module.css';
 
 export default function DashboardPage() {
     return (
         <Dashboard>
-            <div style={{ display: 'flex', flexDirection: 'column', gap: '2rem', alignItems: 'center' }}>
+            <div className={styles.main} style={{ display: 'flex', flexDirection: 'column', gap: '2rem', alignItems: 'center' }}>
                 <TimeLine />
             </div>
         </Dashboard>
